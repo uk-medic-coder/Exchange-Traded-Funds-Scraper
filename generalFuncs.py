230afeeb9f4085c24e6dddad5945a489e80dc69d
uk-medic-coder <117088291+uk-medic-coder@users.noreply.github.com>
2022-11-07 12:58:42 +0000

    Create generalFuncs.py

---
+++ generalFuncs.py
@@ -0,0 +1,26 @@
+
+# general functions
+
+import readline
+import os
+import time
+import datetime
+import json
+
+# ===============================================
+def prefilledInput(prompt, prefill=''):
+   readline.set_startup_hook(lambda: readline.insert_text(prefill))
+   try:
+      return input(prompt)  # or raw_input in Python 2
+   finally:
+      readline.set_startup_hook()
+
+# ===============================================
+
+def SaveFile(fnuse, mode, tx):
+
+    file1 = open(fnuse, mode)
+    file1.write(tx+"\n")
+    file1.close()     
+    
+# ===============================================
